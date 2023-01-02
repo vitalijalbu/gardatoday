e784310f0617d5dc0778280e1c8b6ba602ad16f7
Vitalie Jalbu <40239902+vitalijalbu@users.noreply.github.com>
2023-01-02 21:51:54 +0100

    Create demo.jsx
    demo page

---
+++ src/pages/demo.jsx
@@ -0,0 +1,230 @@
+import React, { useState, useEffect } from "react";
+
+const Archive = () => {
+  return (
+    
+   <div className="page">
+    <div className="uk-section-default uk-section uk-padding-remove-bottom">
+  <div className="uk-container uk-container-xlarge">
+    <div
+      className="tm-grid-expand uk-grid-column-medium uk-grid-divider uk-margin-large uk-grid uk-grid-stack"
+      uk-grid=""
+    >
+      <div className="uk-width-1-2@l uk-width-3-5@xl uk-first-column">
+        <div className="uk-margin">
+          <a
+            className="el-link"
+            href="/joomla/themes/paladin/travel/destinations/235-take-a-romantic-break-in-a-boutique-hotel"
+          >
+            <picture>
+              <source
+                type="image/webp"
+                srcSet="https://via.placeholder.com/800x533"
+                sizes="(min-width: 1600px) 1600px"
+              />
+              <img
+                src="https://via.placeholder.com/800x533"
+                width={1600}
+                height={860}
+                className="el-image"
+                alt=""
+                loading="eager"
+              />
+            </picture>
+          </a>
+        </div>
+        <div className="uk-h6 uk-text-primary uk-margin-remove-bottom uk-text-center">
+          {" "}
+          Boutique hotel{" "}
+        </div>
+        <h2 className="uk-heading-small uk-margin-small uk-width-2xlarge uk-margin-auto uk-text-center">
+          {" "}
+          <a
+            className="el-link uk-link-heading"
+            href="/joomla/themes/paladin/travel/destinations/235-take-a-romantic-break-in-a-boutique-hotel"
+          >
+            Take a romantic break in a boutique hotel
+          </a>{" "}
+        </h2>
+        <div className="uk-panel uk-margin-medium uk-width-xlarge uk-margin-auto uk-text-center">
+          Malesuada bibendum arcu vitae elementum curabitur vitae nunc sed velit
+          dignissim sodales ut eu sem integer eget magna fermentum iaculis eu
+          non diam.
+        </div>
+      </div>
+      <div className="uk-width-1-2@l uk-width-2-5@xl uk-grid-margin uk-first-column">
+        <div className="uk-margin">
+          <div
+            className="uk-child-width-1-1 uk-child-width-1-3@s uk-child-width-1-1@l uk-grid-column-small uk-grid-divider uk-grid-match uk-grid uk-grid-stack"
+            uk-grid=""
+          >
+            {" "}
+            <div className="uk-first-column">
+              <div className="el-item uk-panel uk-margin-remove-first-child">
+                <div
+                  className="uk-child-width-expand uk-grid uk-grid-stack"
+                  uk-grid=""
+                >
+                  {" "}
+                  <div className="uk-width-1-3@l uk-flex-last@l uk-first-column">
+                    <a
+                      href="/joomla/themes/paladin/travel/travel-report/280-african-wildlife-family-safari-vacation-in-kenya-and-tanzania"
+                      aria-label="African wildlife family safari vacation in Kenya and Tanzania"
+                    >
+                      <picture>
+                        <source
+                          type="image/webp"
+                          srcSet="https://via.placeholder.com/800x533"
+                          sizes="(min-width: 610px) 610px"
+                        />
+                        <img
+                          src="https://via.placeholder.com/800x533"
+                          width={610}
+                          height={381}
+                          className="el-image"
+                          alt=""
+                          loading="eager"
+                        />
+                      </picture>
+                    </a>
+                  </div>{" "}
+                  <div className="uk-margin-remove-first-child uk-first-column uk-grid-margin">
+                    <div className="el-meta uk-h6 uk-text-primary uk-margin-top uk-margin-remove-bottom">
+                      Wildlife safari
+                    </div>
+                    <h2 className="el-title uk-h3 uk-margin-small-top uk-margin-remove-bottom">
+                      {" "}
+                      <a
+                        href="/joomla/themes/paladin/travel/travel-report/280-african-wildlife-family-safari-vacation-in-kenya-and-tanzania"
+                        className="uk-link-heading"
+                      >
+                        African wildlife family safari vacation in Kenya and
+                        Tanzania
+                      </a>{" "}
+                    </h2>
+                    <div className="el-content uk-panel uk-margin-small-top">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                      sed do eiusmod tempor incididunt ut labore et dolore magna
+                      aliqua. Ut enim ad minim veniam.
+                    </div>
+                  </div>{" "}
+                </div>
+              </div>
+            </div>
+            <div className="uk-grid-margin uk-first-column">
+              <div className="el-item uk-panel uk-margin-remove-first-child">
+                <div
+                  className="uk-child-width-expand uk-grid uk-grid-stack"
+                  uk-grid=""
+                >
+                  {" "}
+                  <div className="uk-width-1-3@l uk-flex-last@l uk-first-column">
+                    <a
+                      href="/joomla/themes/paladin/travel/destinations/284-the-most-scenic-hikes-and-campsites-around-the-world"
+                      aria-label="The most scenic hikes and campsites around the world"
+                    >
+                      <picture>
+                        <source
+                          type="image/webp"
+                          srcSet="https://via.placeholder.com/800x533"
+                          sizes="(min-width: 610px) 610px"
+                        />
+                        <img
+                          src="https://via.placeholder.com/800x533"
+                          width={610}
+                          height={381}
+                          className="el-image"
+                          alt=""
+                          loading="eager"
+                        />
+                      </picture>
+                    </a>
+                  </div>{" "}
+                  <div className="uk-margin-remove-first-child uk-first-column uk-grid-margin">
+                    <div className="el-meta uk-h6 uk-text-primary uk-margin-top uk-margin-remove-bottom">
+                      Hiking and camping
+                    </div>
+                    <h2 className="el-title uk-h3 uk-margin-small-top uk-margin-remove-bottom">
+                      {" "}
+                      <a
+                        href="/joomla/themes/paladin/travel/destinations/284-the-most-scenic-hikes-and-campsites-around-the-world"
+                        className="uk-link-heading"
+                      >
+                        The most scenic hikes and campsites around the world
+                      </a>{" "}
+                    </h2>
+                    <div className="el-content uk-panel uk-margin-small-top">
+                      Adipiscing elit duis tristique sollicitudin nibh sit amet
+                      commodo nulla facilisi nullam vehicula ipsum a arcu cursus
+                      vitae congue mauris vel elit scelerisque.
+                    </div>
+                  </div>{" "}
+                </div>
+              </div>
+            </div>
+            <div className="uk-grid-margin uk-first-column">
+              <div className="el-item uk-panel uk-margin-remove-first-child">
+                <div
+                  className="uk-child-width-expand uk-grid uk-grid-stack"
+                  uk-grid=""
+                >
+                  {" "}
+                  <div className="uk-width-1-3@l uk-flex-last@l uk-first-column">
+                    <a
+                      href="/joomla/themes/paladin/travel/destinations/282-visiting-iceland-off-the-beaten-track-in-the-land-of-fire-and-ice"
+                      aria-label="Visiting Iceland: Off the beaten track in the land of Fire and Ice"
+                    >
+                      <picture>
+                        <source
+                          type="image/webp"
+                          srcSet="https://via.placeholder.com/800x533"
+                          sizes="(min-width: 610px) 610px"
+                        />
+                        <img
+                          src="https://via.placeholder.com/800x533"
+                          width={610}
+                          height={381}
+                          className="el-image"
+                          alt=""
+                          loading="eager"
+                        />
+                      </picture>
+                    </a>
+                  </div>{" "}
+                  <div className="uk-margin-remove-first-child uk-first-column uk-grid-margin">
+                    <div className="el-meta uk-h6 uk-text-primary uk-margin-top uk-margin-remove-bottom">
+                      Iceland travel routes
+                    </div>
+                    <h2 className="el-title uk-h3 uk-margin-small-top uk-margin-remove-bottom">
+                      {" "}
+                      <a
+                        href="/joomla/themes/paladin/travel/destinations/282-visiting-iceland-off-the-beaten-track-in-the-land-of-fire-and-ice"
+                        className="uk-link-heading"
+                      >
+                        Visiting Iceland: Off the beaten track in the land of
+                        Fire and Ice
+                      </a>{" "}
+                    </h2>
+                    <div className="el-content uk-panel uk-margin-small-top">
+                      Cursus sit amet dictum sit amet justo donec enim diam
+                      vulputate ut pharetra sit amet aliquam id diam maecenas
+                      ultricies mi eget mauris pharetra.
+                    </div>
+                  </div>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+   
+    </div>
+    
+  );
+};
+
+export default Archive;
