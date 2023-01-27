67c26d4fd5628190f5b7a564130bbb0567ef79a4
Vitalie Jalbu <40239902+vitalijalbu@users.noreply.github.com>
2023-01-27 20:07:51 +0100

    events

---
+++ src/pages/eventi/index.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+
+const Index = () => {
+   
+  return (
+    
+   <div className="page">
+   
+        ciao
+    </div>
+    
+  );a
+};
+
+export default Index;
